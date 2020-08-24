9d493cecf75629a6f5b86175b00d5ad20afb7a6f
anenda18 <70150641+anenda18@users.noreply.github.com>
2020-08-24 22:12:36 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+package class=tutorial;
+public class main {
+    public static void main(string[]args)
+    String Nama="ANENDA OCTAVIA SALSA BILLA";
+    String jk="PEREMPUAN";
+    String alamat="DESA GERBO";
+    String jurusan="REKAYASA PERANGKAT LUNAK";
+
+    System.out.println("BIODATA DIRI");
+    System.out.println("Nama \t\t\t\t :"+nama);
+    System.out.println("Jenis kelamin \t\t :"+jk);
+    System.out.println("Alamat \t\t\t\t :"+alamat);ss
+    System.out.println("Jurusan \t\t\t :"+jurusan);
+    
+    
+}
