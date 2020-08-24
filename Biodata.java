8d03e36c274b2690e92f5cd8bfffb8f24e0e086a
anenda18 <70150641+anenda18@users.noreply.github.com>
2020-08-24 23:18:53 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    String Nama="ANENDA OCTAVIA SALSA BILLA";
+    String jk="PEREMPUAN";
+    String alamat="DESA GERBO";
+    String jurusan="REKAYASA PERANGKAT LUNAK";
+
+    System.out.println("BIODATA DIRI");
+    System.out.println("--------");
+    System.out.println("Nama \t\t\t\t :"+Nama);
+    System.out.println("Jenis kelamin \t\t :"+jk);
+    System.out.println("Alamat \t\t\t\t :"+alamat);
+    System.out.println("Jurusan \t\t :"+jurusan);
+    
+    
+}
+    
+    }
+    
+}
